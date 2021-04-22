c4e45211051ff9abd7e55e4da1e968e431cf4aff
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-22 06:56:46 +0000

    [Add]画像表示

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9f29c5f1850eb27b83c1cad950435b2fc85c61319083dff5c0bbd05e39159640ea4beab0733d2e641d35c3a17eb0be83d3e4a0571eb01bc369417197232d4551'
