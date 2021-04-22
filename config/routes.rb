f597a721381572b71eec677b59ded6350cd867cb
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-22 05:52:34 +0000

    [Add]indexからshouw

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'  # HTTPメソッド 'URL' => 'コントローラ#アクション'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show',as:'todolist'
 end
